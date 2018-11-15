6f5953e261c2466a2c8323ac746d75567a5886f0
Ortwin De Witte <odewitte@ctg.com>
2018-11-15 18:31:18 +0100

    Add sum logic, controller and unit test

---
+++ src/main/java/com/or3win/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.or3win.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+         int sum(int a, int b) {
+            return a + b;
+        }
+}
