7e30fb20806880c39ae4051aa904c8ac914fb2a4
Ortwin De Witte <odewitte@ctg.com>
2018-11-25 20:29:00 +0100

    Added javadoc to calculator.java

--- src/main/java/com/or3win/calculator/Calculator.java
+++ src/main/java/com/or3win/calculator/Calculator.java
@@ -2,6 +2,10 @@ package com.or3win.calculator;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * kaka javadoc
+ */
+
 @Service
 public class Calculator {
          int sum(int a, int b) {
